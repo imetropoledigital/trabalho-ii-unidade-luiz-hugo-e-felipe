3487b031c98809e582992fae89d57c6b165ae756
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-01-04 20:58:40 -0300

    feat: :sparkles: fazer paginaçao

--- src/apiController.ts
+++ src/apiController.ts
@@ -1,85 +1,162 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { ApiService } from "./apiService";
 
 /**
  * TODO:
  * ROTA DE GET BY ID (CHECK)
- * IMPLEMENTAR PAGINAÇÃO 
+ * IMPLEMENTAR PAGINAÇÃO
  * CRIAR ROTA DE PUT (CHECK)
  * IMPLEMENTAR PROJEÇÃO (CHECK)
  */
 export const ApiController = {
-  // POST /{collection_name}
-  insertToCollection: async (req: Request, res: Response) => {
-    try {
-      const document = req.body;
-      const collection = req.params.collection;
+    // POST /{collection_name}
+    insertToCollection: async (req: Request, res: Response) => {
+        try {
+            const document = req.body;
+            const collection = req.params.collection;
 
-      if (!document || typeof document != 'object' || Array.isArray(document))
-        return res.status(400).send("É obrigatório passar um JSON no corpo da requisição");
+            if (
+                !document ||
+                typeof document != "object" ||
+                Array.isArray(document)
+            )
+                return res
+                    .status(400)
+                    .send(
+                        "É obrigatório passar um JSON no corpo da requisição"
+                    );
 
-      const createdDocument = await ApiService.insertDocument({document, collection});
-      return res.status(201).send(createdDocument);
-    } catch (error: any) {
-      if (error instanceof Error) return res.status(500).json({ message: error.message });
-    }
-  },
+            const createdDocument = await ApiService.insertDocument({
+                document,
+                collection,
+            });
+            return res.status(201).send(createdDocument);
+        } catch (error: any) {
+            if (error instanceof Error)
+                return res.status(500).json({ message: error.message });
+        }
+    },
 
-  findFromCollection: async (req: Request, res: Response) => {
-    try {
-      const { collection } = req.params;
-      const { query, fields } = req.query
+    findFromCollection: async (req: Request, res: Response) => {
+        try {
+            const { collection } = req.params;
+            const { query, fields, skip, limit } = req.query;
 
-      if (!collection) {
-        return res.status(400).send("O parâmetro 'collection' é obrigatório.");
-      }
-      
-      const findQuery = query ? JSON.parse(query as string) : {};
-      let findFields = [];
+            const skipValue = skip ? Number(skip) : null;
+            const limitValue = limit ? Number(limit) : null;
 
-      if(Array.isArray(fields)) {
-        findFields = fields;
-      }else if(fields && typeof fields === 'string') {
-        findFields = fields.split(',');
-      }
+            if (!collection) {
+                return res
+                    .status(400)
+                    .send("O parâmetro 'collection' é obrigatório.");
+            }
 
-      if (typeof findQuery != 'object' || Array.isArray(findQuery))
-        return res.status(400).send("É obrigatório que a consulta esteja no formato de um JSON");
+            if (isNaN(skipValue) || isNaN(limitValue)) {
+                return res
+                    .status(400)
+                    .send(
+                        "Os parâmetros 'skip' e 'limit' devem ser números válidos."
+                    );
+            }
 
-      const documents = await ApiService.findDocuments({collection , filters: findQuery, fields: findFields});
-      return res.status(200).send(documents);
-    } catch (error: any) {
-      return res.status(500).json({ message: error.message ?? 'Erro desconhecido' })  
-    }
-  },
-  findByIdFromCollection: async(req: Request, res: Response) => {
-    try {
-      const { collection, id } = req.params;
+            if (skipValue < 0 || limitValue < 0) {
+                return res
+                    .status(400)
+                    .send(
+                        "Os parâmetros 'skip' e 'limit' devem ser números positivos."
+                    );
+            }
 
-      const document = await ApiService.findDocumentById({id, collection});
-      
-      if(!document) return res.status(404).send("Não foi encontrado nenhuma entidade desse tipo com esse ID");
-  
-      return res.status(200).json(document);
-    } catch (error: any) {
-      if (error instanceof Error) return res.status(500).json({ message: error.message })
-    }
-  },
-  updateDocumentById: async (req: Request, res: Response) => {
-    try {
-      const { collection, id } = req.params;
-      const document = req.body;
+            const findQuery = query ? JSON.parse(query as string) : {};
+            let findFields = [];
 
-      if (!document || typeof document != 'object' || Array.isArray(document))
-        return res.status(400).send("É obrigatório passar um JSON no corpo da requisição");
+            if (Array.isArray(fields)) {
+                findFields = fields;
+            } else if (fields && typeof fields === "string") {
+                findFields = fields.split(",");
+            }
 
-      const updatedDocument = await ApiService.updateDocumentById({ id, collection, document });
+            if (typeof findQuery != "object" || Array.isArray(findQuery))
+                return res
+                    .status(400)
+                    .send(
+                        "É obrigatório que a consulta esteja no formato de um JSON"
+                    );
 
-      if (!updatedDocument) return res.status(404).send("Não foi encontrado nenhuma entidade desse tipo com esse ID");
+            const pagination = {
+                skip: skipValue,
+                limit: limitValue,
+            };
 
-      return res.status(200).json(updatedDocument);
-    } catch (error: any) {
-      if (error instanceof Error) return res.status(500).json({ message: error.message })
-    }    
-  }
-}
+            const documents = await ApiService.findDocuments({
+                collection,
+                filters: findQuery,
+                fields: findFields,
+                pagination: pagination,
+            });
+
+            return res.status(200).send(documents);
+        } catch (error: any) {
+            return res
+                .status(500)
+                .json({ message: error.message ?? "Erro desconhecido" });
+        }
+    },
+    findByIdFromCollection: async (req: Request, res: Response) => {
+        try {
+            const { collection, id } = req.params;
+
+            const document = await ApiService.findDocumentById({
+                id,
+                collection,
+            });
+
+            if (!document)
+                return res
+                    .status(404)
+                    .send(
+                        "Não foi encontrado nenhuma entidade desse tipo com esse ID"
+                    );
+
+            return res.status(200).json(document);
+        } catch (error: any) {
+            if (error instanceof Error)
+                return res.status(500).json({ message: error.message });
+        }
+    },
+    updateDocumentById: async (req: Request, res: Response) => {
+        try {
+            const { collection, id } = req.params;
+            const document = req.body;
+
+            if (
+                !document ||
+                typeof document != "object" ||
+                Array.isArray(document)
+            )
+                return res
+                    .status(400)
+                    .send(
+                        "É obrigatório passar um JSON no corpo da requisição"
+                    );
+
+            const updatedDocument = await ApiService.updateDocumentById({
+                id,
+                collection,
+                document,
+            });
+
+            if (!updatedDocument)
+                return res
+                    .status(404)
+                    .send(
+                        "Não foi encontrado nenhuma entidade desse tipo com esse ID"
+                    );
+
+            return res.status(200).json(updatedDocument);
+        } catch (error: any) {
+            if (error instanceof Error)
+                return res.status(500).json({ message: error.message });
+        }
+    },
+};
--- src/apiService.ts
+++ src/apiService.ts
@@ -1,58 +1,105 @@
-import { MongoConnection } from "./infra/databases/mongodb/MongoConnection"
+import { MongoConnection } from "./infra/databases/mongodb/MongoConnection";
 import { ObjectId, WithoutId } from "mongodb";
 
 export const ApiService = {
-  insertDocument: async ({document, collection} :{document: WithoutId<Document>, collection: string}) => {
-    const connection = await MongoConnection.getInstance();
+    insertDocument: async ({
+        document,
+        collection,
+    }: {
+        document: WithoutId<Document>;
+        collection: string;
+    }) => {
+        const connection = await MongoConnection.getInstance();
 
-    try {
-      const result = await connection.collection(collection).insertOne(document);
-      console.log("Document inserted with _id:", result.insertedId);
+        try {
+            const result = await connection
+                .collection(collection)
+                .insertOne(document);
+            console.log("Document inserted with _id:", result.insertedId);
 
-      // Adiciona o _id ao documento original e retorna
-      const savedDocument = { ...document, _id: result.insertedId };
-      
-      return savedDocument;      
-    } catch (error) {
-      console.error("Error inserting document:", error);
-      throw error;
-    }
-  },
-  
-  findDocuments: async ({collection, filters, fields}:{collection: string, filters: Object, fields: string[]}) => {
-    const connection = await MongoConnection.getInstance();
+            // Adiciona o _id ao documento original e retorna
+            const savedDocument = { ...document, _id: result.insertedId };
 
-    const projection = fields.reduce((acc, field) => {
-      acc[field] = 1;
-      return acc;
-    }, { _id: 0 });
-    
+            return savedDocument;
+        } catch (error) {
+            console.error("Error inserting document:", error);
+            throw error;
+        }
+    },
 
-    try {
-      return await connection.collection(collection).find(filters, { projection: projection}).toArray();
-    } catch (error) {
-      console.error("Error reading documents:", error);
-      throw error;
-    }    
-  },
-  findDocumentById: async ({id, collection}:{id: string, collection: string}) => {
-    const connection = await MongoConnection.getInstance();
-    try {
-      return await connection.collection(collection).findOne({"_id": new ObjectId(id)});
-    } catch (error) {
-      console.error("Error reading document by id:", error);
-      throw error;
-    }     
-  },
+    findDocuments: async ({
+        collection,
+        filters,
+        fields,
+        pagination,
+    }: {
+        collection: string;
+        filters: Object;
+        fields: string[];
+        pagination: { skip: number | null; limit: number | null };
+    }) => {
+        const connection = await MongoConnection.getInstance();
 
-  updateDocumentById: async ({id, document, collection} : {id: string, document: WithoutId<Document>, collection: string}) => {
-    const connection = await MongoConnection.getInstance();
-    try {
-      const objectId = new ObjectId(id);
-      return await connection.collection(collection).findOneAndReplace({"_id": objectId}, document, {returnDocument: "after"});
-    } catch (error) {
-      console.error("Error updating document by id:", error);
-      throw error;
-    }       
-  }
-}
+        const projection = fields.reduce(
+            (acc, field) => {
+                acc[field] = 1;
+                return acc;
+            },
+            { _id: 0 }
+        );
+
+        try {
+            return await connection
+                .collection(collection)
+                .find(filters, {
+                    projection: projection,
+                    skip: pagination?.skip,
+                    limit: pagination?.limit,
+                })
+                .toArray();
+        } catch (error) {
+            console.error("Error reading documents:", error);
+            throw error;
+        }
+    },
+    findDocumentById: async ({
+        id,
+        collection,
+    }: {
+        id: string;
+        collection: string;
+    }) => {
+        const connection = await MongoConnection.getInstance();
+        try {
+            return await connection
+                .collection(collection)
+                .findOne({ _id: new ObjectId(id) });
+        } catch (error) {
+            console.error("Error reading document by id:", error);
+            throw error;
+        }
+    },
+
+    updateDocumentById: async ({
+        id,
+        document,
+        collection,
+    }: {
+        id: string;
+        document: WithoutId<Document>;
+        collection: string;
+    }) => {
+        const connection = await MongoConnection.getInstance();
+        try {
+            const objectId = new ObjectId(id);
+            return await connection
+                .collection(collection)
+                .findOneAndReplace({ _id: objectId }, document, {
+                    returnDocument: "after",
+                });
+        } catch (error) {
+            console.error("Error updating document by id:", error);
+            throw error;
+        }
+    },
+};
