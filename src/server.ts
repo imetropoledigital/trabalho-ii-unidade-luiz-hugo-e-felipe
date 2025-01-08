86a5700b08651a87b05a29b25e3ac15e5e6cc04d
Fellpz9 <zanathann@gmail.com>
2025-01-08 12:48:53 -0300

    Teste hello world para o caminho raiz

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(router);
 
+app.get('/', (req,res)=>{
+    res.send("Hello, world!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
     MongoConnection.getInstance();
